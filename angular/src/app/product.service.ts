779a5582c0fafd0f104cc7a730202abf27c3d0e8
Milan Kiszely <milankiszely@gmail.com>
2018-10-04 13:31:45 +0200

    extended with getting product details, and getting products in cart

--- angular/src/app/product.service.ts
+++ angular/src/app/product.service.ts
@@ -2,14 +2,30 @@ import { Injectable } from '@angular/core';
 import { Observable, of } from 'rxjs';
 import {Product} from "./product";
 import {PRODUCTS} from "./mock-products";
+import {Cart} from "./mock-cart";
+import {HttpClient, HttpHeaders} from "@angular/common/http";
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductService {
+  private productsURL;
+
   getProducts(): Observable<Product[]> {
-    return of(PRODUCTS);
+    return this.http.get<Product[]>(this.productsURL);
   }
 
-  constructor() { }
+  getProduct(id: number): Observable<Product>{
+    return of(PRODUCTS.find(product => product.id == id));
+  }
+
+  getCart(): Observable<Product[]>{
+    return of (Cart.getInstance().products)
+  }
+
+  addProductToCart(product: Product): void {
+    Cart.getInstance().addToCart(product);
+  }
+
+  constructor(private http: HttpClient) { }
 }
