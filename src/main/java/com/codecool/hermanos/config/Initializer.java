56d602d032f886d52f4637a13ffcfcd039c88502
Unknown <tibormaron@gmail.com>
2018-10-17 11:25:00 +0200

    added backend for registration

--- src/main/java/com/codecool/hermanos/config/Initializer.java
+++ src/main/java/com/codecool/hermanos/config/Initializer.java
@@ -19,13 +19,13 @@ public class Initializer {
     }
 
     private void initializeProducts() {
-        Product product1 = new Product("Crystal Meth", "From Heisenberg's kitchen", 10.0, ProductCategory.DRUG);
+        Product product1 = new Product("Crystal Meth", "From Heisenberg's kitchen", 10.0, "product01", ProductCategory.DRUG);
         pd.addNewProduct(product1);
 
-        Product product2 = new Product("Corn Soup", "A soup made from corn, with tortillas and cheese.", 4.99, ProductCategory.BEFORE);
+        Product product2 = new Product("Corn Soup", "A soup made from corn, with tortillas and cheese.", 4.99,"product02", ProductCategory.BEFORE);
         pd.addNewProduct(product2);
 
-        Product product3 = new Product("Cheese Soup", "Made from the three best mexican cheese with some Jalapenos.", 4.99, ProductCategory.BEFORE);
+        Product product3 = new Product("Cheese Soup", "Made from the three best mexican cheese with some Jalapenos.", 4.99, "product01", ProductCategory.BEFORE);
         pd.addNewProduct(product3);
     }
 }
