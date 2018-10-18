33b9aa9f347ff58f2d7522572a283f8432d1ea0a
Unknown <tibormaron@gmail.com>
2018-10-18 11:29:12 +0200

    fixed category selector in ProductController

--- src/main/java/com/codecool/hermanos/controller/ProductController.java
+++ src/main/java/com/codecool/hermanos/controller/ProductController.java
@@ -3,35 +3,34 @@ package com.codecool.hermanos.controller;
 import com.codecool.hermanos.dao.DaoProduct;
 import com.codecool.hermanos.model.ProductCategory;
 import com.google.gson.Gson;
-import com.google.gson.JsonSyntaxException;
 
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.EnumSet;
 import java.util.List;
 
 public class ProductController extends HttpServlet {
 
     private DaoProduct daoProduct;
+    private EnumSet<ProductCategory> categories;
 
-    public ProductController(DaoProduct daoProduct) {
+    public ProductController(DaoProduct daoProduct, EnumSet<ProductCategory> categories) {
         this.daoProduct = daoProduct;
+        this.categories = categories;
     }
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
         Gson gson = new Gson();
-
-        String category = request.getParameter("category");
         List products;
 
-        try {
-            ProductCategory productCategory = gson.fromJson(category, ProductCategory.class);
-            products = daoProduct.getProductsByCategory(productCategory);
-        } catch (JsonSyntaxException e) {
-            products = daoProduct.getAllProducts();
-        }
+        String category = request.getParameter("category");
+        ProductCategory productCategory = gson.fromJson(category, ProductCategory.class);
+
+        if (categories.contains(productCategory)) products = daoProduct.getProductsByCategory(productCategory);
+        else products = daoProduct.getAllProducts();
 
         String productsInJson = gson.toJson(products);
         response.getWriter().write(productsInJson);
