8b2542b654b8840d70632997b55de20afd54529f
Unknown <tibormaron@gmail.com>
2018-10-18 10:15:18 +0200

    added ProductCategory to /products page

--- src/main/java/com/codecool/hermanos/config/ServletInitializer.java
+++ src/main/java/com/codecool/hermanos/config/ServletInitializer.java
@@ -1,19 +1,15 @@
 package com.codecool.hermanos.config;
 
-import com.codecool.hermanos.controller.ProductController;
-import com.codecool.hermanos.controller.ProductPageController;
-import com.codecool.hermanos.controller.RegistrationController;
-import com.codecool.hermanos.dao.DaoAddress;
-import com.codecool.hermanos.dao.DaoProduct;
-import com.codecool.hermanos.dao.DaoUsers;
-import com.codecool.hermanos.dao.implementation.DaoAddressJPA;
-import com.codecool.hermanos.dao.implementation.DaoProductJPA;
-import com.codecool.hermanos.dao.implementation.DaoUsersJPA;
+import com.codecool.hermanos.controller.*;
+import com.codecool.hermanos.dao.*;
+import com.codecool.hermanos.dao.implementation.*;
+import com.codecool.hermanos.model.ProductCategory;
 
 import javax.servlet.ServletContext;
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
 import javax.servlet.annotation.WebListener;
+import java.util.EnumSet;
 
 @WebListener
 public class ServletInitializer implements ServletContextListener {
@@ -23,10 +19,11 @@ public class ServletInitializer implements ServletContextListener {
         DaoUsers daoUsers = new DaoUsersJPA();
         DaoAddress daoAddress = new DaoAddressJPA();
         DaoProduct daoProduct = new DaoProductJPA();
+        EnumSet<ProductCategory> categories = EnumSet.allOf(ProductCategory.class);
 
         ServletContext context = sce.getServletContext();
         context.addServlet("Registration", new RegistrationController(daoUsers, daoAddress)).addMapping("/registration");
         context.addServlet("ProductPage", new ProductPageController(daoProduct)).addMapping("/product");
-        context.addServlet("Product", new ProductController(daoProduct)).addMapping("/products");
+        context.addServlet("Product", new ProductController(daoProduct, categories)).addMapping("/products");
     }
 }
--- src/main/java/com/codecool/hermanos/controller/ProductController.java
+++ src/main/java/com/codecool/hermanos/controller/ProductController.java
@@ -1,27 +1,37 @@
 package com.codecool.hermanos.controller;
 
 import com.codecool.hermanos.dao.DaoProduct;
+import com.codecool.hermanos.model.ProductCategory;
 import com.google.gson.Gson;
 
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.EnumSet;
 import java.util.List;
 
 public class ProductController extends HttpServlet {
 
     private DaoProduct daoProduct;
+    private EnumSet<ProductCategory> categories;
 
-    public ProductController(DaoProduct daoProduct) {
+    public ProductController(DaoProduct daoProduct, EnumSet<ProductCategory> categories) {
         this.daoProduct = daoProduct;
+        this.categories = categories;
     }
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-        List products = daoProduct.getAllProducts();
         Gson gson = new Gson();
+        PrintWriter pw = response.getWriter();
+
+        List products = daoProduct.getAllProducts();
         String productsInJson = gson.toJson(products);
-        response.getWriter().write(productsInJson);
+        pw.write(productsInJson);
+
+        String categoriesInJson = gson.toJson(categories);
+        pw.write(categoriesInJson);
     }
 }
